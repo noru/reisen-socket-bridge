958a97f4430a30972e176e88c87ca8175d410e98
Drew <nooooru@gmail.com>
2018-10-27 12:28:04 +0800

    v1.0 move with web controller

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask
+from flask import Flask, send_from_directory, render_template
 from flask_socketio import SocketIO, emit
+from geometry_msgs.msg import Twist
+import rospy
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+linear_speed = 0.3
+angular_speed = 1.4
+publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+rospy.init_node('reisen_socket_bridge')
+
+def sendTwist(direction):
+    twist = Twist()
+    if ('up' in direction):
+        twist.linear.x = linear_speed
+    if ('down' in direction):
+        twist.linear.x = -linear_speed
+    if ('left' in direction):
+        twist.angular.z = angular_speed
+    if ('right' in direction):
+        twist.angular.z = -angular_speed
+    publisher.publish(twist)
+
+
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1><script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.dev.js"></script>'
+def index():
+    return send_from_directory('', 'index.html')
 
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']})
+@app.route('/assets/<path:path>')
+def send_js(path):
+    return send_from_directory('assets', path)
 
-@socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+@socketio.on('key-pressed')
+def handle_key_press(key):
+    sendTwist(key)
+    emit('response', key)
 
-@socketio.on('connect', namespace='/test')
+
+@socketio.on('connect')
 def test_connect():
     emit('my response', {'data': 'Connected'})
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
---
+++ assets/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function() {
+  var rate = 10 // Hz
+  var socket = io()
+  socket.connect('/')
+  socket.on('connect', () => {
+    console.info('Reisen Socket Bridge connected.')
+  })
+
+  socket.on('response', (m) => {
+    console.info('response: command ' + m + ' received')
+  })
+
+  var intervalId = 0
+  function handleKeyDown(e) {
+    clearInterval(intervalId)
+    intervalId = setInterval(() => {
+      socket.emit('key-pressed', e.target.id)
+    }, 1000 / rate)
+
+  }
+
+  function handleKeyUp() {
+    clearInterval(intervalId)
+  }
+
+  $('.controller-row > div').on('mousedown touchstart', handleKeyDown)
+  $('.controller-row > div').on('mouseup touchend', handleKeyUp)
+
+})()
